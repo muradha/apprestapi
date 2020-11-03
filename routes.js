2ba426ffc392538a6f1f20d3c1af807d9b9f06bd
Mhd Rizki adha <mhdrizki098@gmail.com>
2020-11-03 18:09:55 +0700

    install package jwt, morgan dan ip

--- routes.js
+++ routes.js
@@ -7,12 +7,17 @@ module.exports = function(app){
         .get(jsonku.index);
     app.route('/tampil')
         .get(jsonku.tampilsemuamahasiswa);
+        
     app.route('/tampil/:id')
         .get(jsonku.tampilberdasarkanid);
     app.route('/tambah')
         .post(jsonku.tambahMahasiswa);
+
     app.route('/ubah')
         .put(jsonku.ubahMahasiswa);
     app.route('/hapus')
         .delete(jsonku.hapusMahasiswa);
+
+    app.route('/tampilmatakuliah')
+        .get(jsonku.tampilgroupmatakuliah);
 }
